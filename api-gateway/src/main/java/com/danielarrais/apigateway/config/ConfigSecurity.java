b9dd4bd0ebd1aaacd07fac0e1d13cc24d2db0562
Daniel Arrais <danielarraiscarvalho@gmail.com>
2022-12-07 09:13:10 -0300

    Configure keycloak on api-gateway

---
+++ api-gateway/src/main/java/com/danielarrais/apigateway/config/ConfigSecurity.java
@@ -0,0 +1,26 @@
+package com.danielarrais.apigateway.config;
+
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@CacheConfig
+@EnableWebFluxSecurity
+public class ConfigSecurity {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        serverHttpSecurity.csrf()
+                .disable()
+                .authorizeExchange(exchangeSpec -> exchangeSpec
+                        .pathMatchers("/eureka/**")
+                        .permitAll()
+                        .anyExchange()
+                        .authenticated())
+                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+
+        return serverHttpSecurity.build();
+    }
+}
