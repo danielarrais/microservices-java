9a3c0b071f10e07ea24089d5aa579666e6f5cbfd
Daniel Arrais <danielarraiscarvalho@gmail.com>
2022-12-08 10:38:46 -0300

    Implemented circuit breaker to order place endpoint

--- order-service/src/main/java/com/danielarrais/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/danielarrais/orderservice/controller/OrderController.java
@@ -2,9 +2,12 @@ package com.danielarrais.orderservice.controller;
 
 import com.danielarrais.orderservice.dto.OrderRequest;
 import com.danielarrais.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClientException;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
 
 @RestController
 @RequestMapping("/api/order")
@@ -15,8 +18,13 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "placeOrderFallback")
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
         return "Order placed successfully";
+    }
+
+    public String placeOrderFallback(OrderRequest orderRequest, WebClientException runtimeException) {
+        return "Puts! Something went wrong, please retry later";
     }
 }
